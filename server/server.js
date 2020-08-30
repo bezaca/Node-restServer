2096aa11efb97889953f807487f685a85ba7a71e
Felipe <cabezafelipe@gmail.com>
2020-08-29 21:25:35 -0500

    http requests

--- server/server.js
+++ server/server.js
@@ -9,49 +9,49 @@ const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
- 
+
 // parse application/json
 app.use(bodyParser.json());
 
 app.get('/usuario', function (req, res) {
-    res.json('get Usuario');
+  res.json('get Usuario');
 });
 
 app.post('/usuario', function (req, res) {
 
   let body = req.body;
-  
-  if( body.nombre === undefined){
+
+  if (body.nombre === undefined) {
 
     res.status(400).json({
-      ok : false ,
-      mensaje : 'El nombre es necesario'
+      ok: false,
+      mensaje: 'El nombre es necesario'
     });
 
   } else {
-      
-    res.json({ 
-      persona : body
-    });  
+
+    res.json({
+      persona: body
+    });
   }
-  
+
 
 });
 
 
 app.put('/usuario/:id', function (req, res) {
-// Necesario que el modulo del params sea igual al del url
-    let id = req.params.id;
+  // Necesario que el modulo del params sea igual al del url
+  let id = req.params.id;
 
-    res.json({
-      id   
-    });
+  res.json({
+    id
+  });
 });
 
 app.delete('/usuario', function (req, res) {
-    res.json('delete Usuario');
+  res.json('delete Usuario');
 });
 
 app.listen(process.env.PORT, () => {
   console.log('Escuchando puerto: ', process.env.PORT);
- });
+});
